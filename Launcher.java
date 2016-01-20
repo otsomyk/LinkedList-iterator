8fb1ed4f4db79660332c937037cbf30065a8ee69
otsomyk <otsomyk@users.noreply.github.com>
2016-01-20 13:17:23 +0200

    Initial commit

---
+++ Launcher.java
@@ -0,0 +1,21 @@
+package linkedList;
+
+
+public class Launcher {
+
+	public static void main(String[] args) {
+		 LinkedList list = new LinkedList();
+
+		 list.addFirst("2");
+		 list.addFirst("1");
+		 list.addLast("3");
+		 list.addAfter("3", "4");
+		 list.addAfter("3", "5");
+
+
+	        for(Object o : list) {
+	            System.out.println(o);
+	        }
+	    }
+
+}
